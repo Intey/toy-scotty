e556b39092f05d88508782942b949eba27d320e1
intey <churin.d.r@sberbank.ru>
2020-07-15 08:56:36 +0300

    [up]: 1 Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
-module Main where
-
+    
 import Lib
 
+-- client functions
+constructResponse :: [String] -> String
+constructResponse = unwords
+
+routeHandler1 :: String -> String
+routeHandler1 request = constructResponse [
+    "request in handler1 got: '" ++ request ++ "'"]
+
+routeHandler2 :: String -> String
+routeHandler2 request = constructResponse [
+    "request in handler2 got: '" ++ request ++ "'"]
+
+routeHandler3 :: String -> String
+routeHandler3 request = constructResponse [
+    "request in handler3 got: '" ++ request ++ "'"]
+
+myApp :: AppStateT ()
+myApp = do
+    addRoute routeHandler1
+    addRoute routeHandler2
+    addRoute routeHandler3
+
 main :: IO ()
-main = someFunc
+main = myScotty myApp
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,47 @@
 module Lib
-    ( someFunc
+    ( Route
+    , AppState
+    , AppStateT
+    , addRoute
+    , myScotty
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Control.Monad.Trans.State.Strict as ST
+import Control.Monad
+
+type Route = String -> String
+
+data AppState = AppState { routes::[Route]}
+type AppStateT = ST.State AppState
+
+addRoute' :: Route -> AppState -> AppState
+addRoute' mf s@AppState {routes = mw} = s {routes = mf:mw}
+
+addRoute :: Route -> AppStateT ()
+addRoute mf = ST.modify $ \s -> addRoute' mf s
+
+runMyApp :: Monad m => String -> ST.State AppState a -> m String
+runMyApp request my_app = do
+    -- initialize routes from client
+    let s = ST.execState my_app AppState{ routes = []}
+    -- apply first route to request, then apply it's response to second route and so on
+    let output = foldr ($) request (routes s) 
+    return $ output
+
+userInputLoop :: AppStateT () -> IO ()
+userInputLoop app_state = do
+  putStrLn "Please type in the request"
+  putStrLn "(one of 'handler1', 'handler2', 'handler3', 'buggy' or any string for default handling)"
+  request <- getLine
+
+  unless (request == "q") $ do
+    let response = runMyApp request app_state
+    case response of
+      Just x -> putStrLn x
+      Nothing -> putStrLn "Error"
+    userInputLoop app_state
+
+myScotty :: AppStateT () -> IO ()
+myScotty my_app = do
+  userInputLoop my_app
+  
