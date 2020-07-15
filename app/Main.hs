2edc97480d7fef1e73ff315cd5d3d28dd8c051e8
intey <churin.d.r@sberbank.ru>
2020-07-15 10:28:46 +0300

    [up]: 2 iteration

--- app/Main.hs
+++ app/Main.hs
@@ -2,26 +2,26 @@
 import Lib
 
 -- client functions
-constructResponse :: [String] -> String
+-- constructResponse :: [String] -> String
 constructResponse = unwords
 
-routeHandler1 :: String -> String
+routeHandler1 :: Handler
 routeHandler1 request = constructResponse [
     "request in handler1 got: '" ++ request ++ "'"]
 
-routeHandler2 :: String -> String
+routeHandler2 :: Handler
 routeHandler2 request = constructResponse [
     "request in handler2 got: '" ++ request ++ "'"]
 
-routeHandler3 :: String -> String
+routeHandler3 :: Handler
 routeHandler3 request = constructResponse [
     "request in handler3 got: '" ++ request ++ "'"]
 
 myApp :: AppStateT ()
 myApp = do
-    addRoute routeHandler1
-    addRoute routeHandler2
-    addRoute routeHandler3
+    addRoute routeHandler1 (== "handler1")
+    addRoute routeHandler2 (== "handler2")
+    addRoute routeHandler3 (== "handler3")
 
 main :: IO ()
 main = myScotty myApp
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     ( Route
     , AppState
     , AppStateT
+    , Handler
     , addRoute
     , myScotty
     ) where
@@ -9,39 +10,50 @@ module Lib
 import qualified Control.Monad.Trans.State.Strict as ST
 import Control.Monad
 
-type Route = String -> String
+type Handler = String -> String
+type Route = Handler -> Handler
 
 data AppState = AppState { routes::[Route]}
 type AppStateT = ST.State AppState
 
+-- Inside: Possible replace
+-- route = flip select
+-- import Control.Conditional from package cond
+-- for remove flip, we should declare route as @(String -> Bool) -> Handler -L Route@
+route :: Handler -> (String -> Bool) -> Route
+route handler1 cond handler2 input_string =
+  if cond input_string then
+    handler1 input_string
+  else
+    handler2 input_string
+
+
 addRoute' :: Route -> AppState -> AppState
 addRoute' mf s@AppState {routes = mw} = s {routes = mf:mw}
 
-addRoute :: Route -> AppStateT ()
-addRoute mf = ST.modify $ \s -> addRoute' mf s
+addRoute :: Handler -> (String -> Bool) -> AppStateT ()
+addRoute mf pat = ST.modify $ \s -> addRoute' (route mf pat) s
 
-runMyApp :: Monad m => String -> ST.State AppState a -> m String
-runMyApp request my_app = do
-    -- initialize routes from client
-    let s = ST.execState my_app AppState{ routes = []}
-    -- apply first route to request, then apply it's response to second route and so on
-    let output = foldr ($) request (routes s) 
-    return $ output
+runMyApp :: Monad m => Handler -> AppState -> String -> m String
+runMyApp def app_state request = do
+  let output = foldl (flip ($)) def (routes app_state) request
+  return output
 
-userInputLoop :: AppStateT () -> IO ()
+defaultRoute request = unwords ["default router got: '" ++ request ++ "'"]
+
 userInputLoop app_state = do
   putStrLn "Please type in the request"
   putStrLn "(one of 'handler1', 'handler2', 'handler3', 'buggy' or any string for default handling)"
   request <- getLine
-
   unless (request == "q") $ do
-    let response = runMyApp request app_state
+    let response = runMyApp defaultRoute app_state request
     case response of
       Just x -> putStrLn x
       Nothing -> putStrLn "Error"
     userInputLoop app_state
 
+
 myScotty :: AppStateT () -> IO ()
 myScotty my_app = do
-  userInputLoop my_app
-  
+    let app_state = ST.execState my_app AppState{ routes = []}
+    userInputLoop app_state  
